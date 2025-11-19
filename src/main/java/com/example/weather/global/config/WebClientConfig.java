c0a872319c05c06add2164087fcb70aae36b103b
hyunj24 <hyunjug7@naver.com>
2025-11-20 01:43:29 +0900

    config: WebClientConfig 추가

---
+++ src/main/java/com/example/weather/global/config/WebClientConfig.java
@@ -0,0 +1,16 @@
+package com.example.weather.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("https://api.openweathermap.org") // 기본 URL
+                .build();
+    }
+}
